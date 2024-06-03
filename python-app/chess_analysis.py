af8167a35da148bfd36fb59dc840858888ace018
guilhermefranca <guilhermefrn71@gmail.com>
2024-06-02 22:32:58 -0300

    .

--- python-app/chess_analysis.py
+++ python-app/chess_analysis.py
@@ -4,7 +4,7 @@ import random
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, PosicaoInicial, Sequencia
+from models import Base, Posicao, Sequencia
 
 # Configuração do Stockfish
 engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
@@ -29,20 +29,35 @@ def get_random_fen():
 
 def evaluate_position(fen):
     board = chess.Board(fen)
-    result = engine.analyse(board, chess.engine.Limit(time=60))
-    return result['score'].relative.score()
+    result = engine.analyse(board, chess.engine.Limit(time=240, depth=240))
+    #return result['score'].relative.score()
+    return result
 
 def main():
-    session = create_connection()
-    Base.metadata.create_all(session.get_bind())  # Cria as tabelas, se não existirem
+    #session = create_connection()
+    #Base.metadata.create_all(session.get_bind())  # Cria as tabelas, se não existirem
 
+    fen = '8/3K3B/4p2P/2p1k1p1/8/p7/8/8 w - - 0 30'
+    try:
+        #fen = 'r1bq3k/ppp1nQp1/4pN1p/6N1/2BP4/2P1n3/PP4PP/R5K1 w - - 0 30'
+        evaluation = evaluate_position(fen)
+        pv_moves = evaluation['pv']
+        best_moves = [move.uci() for move in pv_moves]
+
+        print(best_moves)
+        
+        #print("Best sequence of moves:", evaluation)
+    except chess.engine.EngineError as e:
+        print(f"Engine error: {e}")
+
+    '''
     random_fen = get_random_fen()
     initial_evaluation = evaluate_position(random_fen)
     print(initial_evaluation)
     
     # Inserir posição inicial
-    posicao_inicial = PosicaoInicial(fen=random_fen, avaliacao=initial_evaluation)
-    session.add(posicao_inicial)
+    posicao = Posicao(fen=random_fen, avaliacao=initial_evaluation)
+    session.add(posicao)
     session.commit()
 
     # Gerar sequências
@@ -51,10 +66,11 @@ def main():
         board.push(move)
         fen = board.fen()
         evaluation = evaluate_position(fen)
-        sequencia = Sequencia(id_posicao_inicial=posicao_inicial.id, fen=fen, avaliacao=evaluation)
+        sequencia = Sequencia(id_posicao=posicao.id, fen=fen, avaliacao=evaluation)
         session.add(sequencia)
         session.commit()
         board.pop()
+    '''
 
 if __name__ == "__main__":
     main()
--- python-app/models.py
+++ python-app/models.py
@@ -4,17 +4,29 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class PosicaoInicial(Base):
-    __tablename__ = 'posicoes_iniciais'
+class Posicao(Base):
+    __tablename__ = 'posicoes'
     id = Column(Integer, primary_key=True, autoincrement=True)
     fen = Column(String(255), nullable=False)
     avaliacao = Column(Float, nullable=False)
-    sequencias = relationship("Sequencia", back_populates="posicao_inicial")
+    qwp = Column(Integer, nullable=False)
+    qbp = Column(Integer, nullable=False)
+    qwr = Column(Integer, nullable=False)
+    qbr = Column(Integer, nullable=False)
+    qwn = Column(Integer, nullable=False)
+    qbn = Column(Integer, nullable=False)
+    qwb = Column(Integer, nullable=False)
+    qbb = Column(Integer, nullable=False)
+    qbq = Column(Integer, nullable=False)
+    qbq = Column(Integer, nullable=False)
+    sequencias = relationship("Sequencia", back_populates="posicoes")
 
 class Sequencia(Base):
-    __tablename__ = 'sequencia'
+    __tablename__ = 'sequencias'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_posicao_inicial = Column(Integer, ForeignKey('posicoes_iniciais.id'))
+    id_posicao = Column(Integer, ForeignKey('posicoes.id'))
+    ordem = Column(Integer, nullable=False)
     fen = Column(String(255), nullable=False)
     avaliacao = Column(Float, nullable=False)
-    posicao_inicial = relationship("PosicaoInicial", back_populates="sequencias")
+    mate = Column(Integer, nullable=False, default=0)
+    posicao = relationship("Posicao", back_populates="sequencias")
