7f6a407e270dd350dfa99165051224d35e7a9776
guilhermefranca <guilhermefrn71@gmail.com>
2024-06-08 14:36:45 -0300

    comentarios

--- python-app/chess_analysis.py
+++ python-app/chess_analysis.py
@@ -8,7 +8,7 @@ from models import Base, Posicao, Sequencia
 import time
 
 
-# Configuração do Stockfish
+# Inicia o stockfish com o path de dentro dentro do container
 engine_path = "/usr/games/stockfish"
 engine = chess.engine.SimpleEngine.popen_uci(engine_path)
 
@@ -23,23 +23,23 @@ def create_connection():
     Session = sessionmaker(bind=engine)
     return Session()
 
-def get_random_fen():
+# cria um FEN válido aleatório
+def get_random_fen(): ## ACHO QUE NÃO VOU UTILIZAR
     board = chess.Board()
     for _ in range(random.randint(1, 20)):
         move = random.choice(list(board.legal_moves))
         board.push(move)
     return board.fen()
 
-def evaluate_position(fen):
-    board = chess.Board(fen)
-    result = engine.analyse(board, chess.engine.Limit(time=240, depth=240))
-    #return result['score'].relative.score()
-    return result
 
-def get_best_lines(board, depth=30):
-    info = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=1)
+# Aparentemente o stockfish guarda em cache avaliações de uma posição
+# então se eu chamar essa função várias vezes para a mesma posição
+# Ele vai sempre melhorar a análise
+def get_best_lines(board, depth=20, multipv=3):
+    info = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=multipv)
     return info
 
+# Mostra as linhas de uma forma menos poluida
 def get_line_str(line):
     line_str = ', '.join([move.uci() for move in line])
     return line_str
@@ -64,53 +64,6 @@ def main():
             time.sleep(1)
     except Exception as e:
         print(f"Engine error: {e} ")
-
-    '''
-    try:
-        fen = '8/3K3B/4p2P/2p1k1p1/8/p7/8/8 w - - 0 30'
-        board = chess.Board(fen)
-
-        best_lines = get_best_lines(board, depth=10)
-        for i, line_info in enumerate(best_lines):
-            score = line_info['score'].relative.score(mate_score=10000)
-            line = line_info['pv']
-            line_str = " ".join([board.san(move) for move in line])
-            print(f"Line {i+1}: Score = {score}, Moves = {line_str}")
-    except Exception as e:
-        print(f"Engine error: {e} ")
-
-    try:
-        #fen = 'r1bq3k/ppp1nQp1/4pN1p/6N1/2BP4/2P1n3/PP4PP/R5K1 w - - 0 30'
-        evaluation = evaluate_position(fen)
-        pv_moves = evaluation['pv']
-        best_moves = [move.uci() for move in pv_moves]
-        print(best_moves)
-        
-        #print("Best sequence of moves:", evaluation)
-    except chess.engine.EngineError as e:
-        print(f"Engine error: {e}")
-
-    
-    random_fen = get_random_fen()
-    initial_evaluation = evaluate_position(random_fen)
-    print(initial_evaluation)
-    
-    # Inserir posição inicial
-    posicao = Posicao(fen=random_fen, avaliacao=initial_evaluation)
-    session.add(posicao)
-    session.commit()
-
-    # Gerar sequências
-    board = chess.Board(random_fen)
-    for move in board.legal_moves:
-        board.push(move)
-        fen = board.fen()
-        evaluation = evaluate_position(fen)
-        sequencia = Sequencia(id_posicao=posicao.id, fen=fen, avaliacao=evaluation)
-        session.add(sequencia)
-        session.commit()
-        board.pop()
-    '''
 
 if __name__ == "__main__":
     main()
